02fc9cfd3ca93e6c5aabf5f1ce759e4c8ce22999
bronivichok1 <lazarenkov01@gmail.com>
2024-01-29 15:25:05 +0300

    0.3.1

---
+++ ConnectSQL.php
@@ -0,0 +1,29 @@
+<?php
+$host='localhost';
+$db = 'FreeBot';
+$username = 'dbuser';
+$password = 'admin';
+ 
+# Создаем соединение с базой PostgreSQL с указанными выше параметрами
+$dbconn = pg_connect("host=$host port=5432 dbname=$db user=$username password=$password");
+ 
+if (!$dbconn) {
+die('Could not connect');
+}
+else {
+echo ("Connected to local DB");
+
+# Сделаем запрос на получение списка созданных таблиц
+$res = pg_query($dbconn, "select table_name, column_name from information_schema.columns where table_schema='public'");
+if (!$res) {
+    echo "Произошла ошибка.\n";
+
+}
+}
+# Выведем список таблиц и столбцов в каждой таблице
+
+while ($row = pg_fetch_row($res)) {
+    echo "tableName: $row[0] ColumnName: $row[1]";
+    echo "<br />\n";
+}
+?>
